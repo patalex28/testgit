91498d2559bf17e71eee7a4206ff745cb978f1ed
acandelo <acandelo@TEUTATES.lpce.lan>
2015-08-19 09:23:33 +0200

    test premier commit

---
+++ Essaikata/src/main/java/com/kata/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,13 @@
+package com.kata.FizzBuzz;
+
+public class FizzBuzz {
+
+	public static String getResult(int i) {
+		if (i % 15 == 0) return "fizzbuzz";
+		if (i % 5 == 0) return "buzz";
+		if (i % 3 == 0) return "fizz";
+		
+		return String.valueOf(i);
+	}
+
+}
---
+++ Essaikata/src/test/java/com/kata/testFizzBuzz/testFizzBuzz.java
@@ -0,0 +1,34 @@
+package com.kata.testFizzBuzz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.kata.FizzBuzz.FizzBuzz;
+
+public class testFizzBuzz {
+
+	@Test
+    public void getResultShouldReturnFizzIfTheNumberIsDividableBy3() {
+        Assert.assertEquals("fizz", FizzBuzz.getResult(3));
+    }
+ 
+    @Test
+    public void getResultShouldReturnBuzzIfTheNumberIsDividableBy5() {
+        Assert.assertEquals("buzz", FizzBuzz.getResult(5));
+        Assert.assertEquals("buzz", FizzBuzz.getResult(10));
+    }
+ 
+    @Test
+    public void getResultShouldReturnBuzzIfTheNumberIsDividableBy15() {
+        Assert.assertEquals("fizzbuzz", FizzBuzz.getResult(15));
+        Assert.assertEquals("fizzbuzz", FizzBuzz.getResult(30));
+    }
+ 
+    @Test
+    public void getResultShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled() {
+        Assert.assertEquals("1", FizzBuzz.getResult(1));
+        Assert.assertEquals("2", FizzBuzz.getResult(2));
+        Assert.assertEquals("4", FizzBuzz.getResult(4));
+    }
+
+}
